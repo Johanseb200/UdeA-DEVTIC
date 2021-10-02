7835e50ca5d75f7ea04c8ce90189bd74efc15ec4
steven_980213@hotmail.com <steven_980213@hotmail.com>
2021-10-02 07:47:10 -0500

    Solucionar conflicto

--- devtic/src/App.js
+++ devtic/src/App.js
@@ -1,24 +1,48 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
 
-import{
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-
+import Error from './Shared/Error'
+import Home from './Home/pages/Home';
+import Maestro from './Ventas/pages/Maestro';
+import Registro from './Ventas/pages/Registro';
+import Header from "./Shared/Header"
+import Registro_de_productos from "./productos/pages/registro_de_productos";
+import Maestro_de_productos from "./productos/pages/maestro_de_productos";
 import User_master from './user_master/pages/User_master';
 
-function App() {
+const App = () => {
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/User_master" exact>
-          <User_master/>
-        </Route>
-      </Switch>
-    </Router>  
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/registro_de_ventas" exact>
+            <Registro />
+          </Route>
+          <Route path="/maestro_de_ventas" exact>
+            <Maestro />
+          </Route>
+          <Route path="/maestro_de_productos" exact>
+            <Maestro_de_productos />
+          </Route>
+          <Route path="/registro_de_productos" exact>
+            <Registro_de_productos />
+          </Route>
+          <Route path="/User_master" exact>
+            <User_master />
+          </Route>
+          <Route path="/Error" exact>
+            <Error />
+          </Route>
+          <Redirect to="/Error" />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
